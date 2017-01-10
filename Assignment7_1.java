2ace336769dea28f4ce0a410f2375fabf2dba4f8
SubrahmanyaGanapumane <subrahmanyagg@gmail.com>
2017-01-10 11:48:24 +0530

    Add files via upload

---
+++ Assignment7_1.java
@@ -0,0 +1,12 @@
+package session7;
+
+public class Assignment7_1 {
+
+	public static void main(String[] args) {
+		int i=100;
+		
+		String str = String.format ("%d", i); 
+		System.out.println(str);
+	}
+
+}
